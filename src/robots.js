4fdba92b78622f23c9da0806ed9d29f5103ba46c
thevenine <seyeoyeniran2u@gmail.com>
2019-11-19 13:35:09 +0100

    create card component

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [{
+        id: 1,
+        name: 'Seye Oyeniran',
+        email: 'my-email@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Seye Johnson',
+        email: 'my-email@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Taiwo Oyeniran',
+        email: 'my-email@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Bode Oyeniran',
+        email: 'my-email@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Segun Oyeniran',
+        email: 'my-email@gmail.com'
+    },
+]
